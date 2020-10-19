545287696e758c312c2c4005e250e0bc45319336
Tibisfly <vielmatorotibisay@gmail.com>
2020-10-19 16:08:39 +0000

    just the cards

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -4,16 +4,10 @@ export function Card(props) {
 	return (
 		<div className="wrapperCard">
 			<div className="card">
-				<img
-					src={props.myCardsArray[0].src}
-					className="card-img-top"
-					alt="..."
-				/>
+				<img src="www" className="card-img-top" alt="..." />
 				<div className="card-body">
 					<h5 className="card-title">Card title</h5>
-					<p clasclassNames="card-text">
-						{props.myCardsArray[0].text}
-					</p>
+					<p clasclassNames="card-text">Some text</p>
 					<a href="#" className="btn btn-primary">
 						Find out more!
 					</a>
@@ -22,3 +16,26 @@ export function Card(props) {
 		</div>
 	);
 }
+
+// let myCardsArray = [
+// 	{
+// 		src:
+// 			"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR7lWHi3VMn5X78uJH5FMqKrnyQ7Fb8vLKt2w&usqp=CAU",
+// 		text: "Prueba 1"
+// 	},
+// 	{
+// 		src:
+// 			"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSzvIUbKcfOv5WH1RLxcq_Z9FPE38h0pXjEgw&usqp=CAU",
+// 		text: "Prueba 1"
+// 	},
+// 	{
+// 		src:
+// 			"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ7Vh0jU3tQOo0t9_BEAfS_ztfN4-RsyXj-_g&usqp=CAU",
+// 		text: "Prueba 1"
+// 	},
+// 	{
+// 		src:
+// 			"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQDwgWJoRxJOdrMhD_rJRGgTow3uvE3ZryOTA&usqp=CAU",
+// 		text: "Prueba 1"
+// 	}
+// ];
--- src/js/component/home.js
+++ src/js/component/home.js
@@ -10,36 +10,13 @@ import { Card } from "./card.js";
 import { Footer } from "./footer.js";
 
 export function Home(props) {
-	let myCardsArray = [
-		{
-			src:
-				"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR7lWHi3VMn5X78uJH5FMqKrnyQ7Fb8vLKt2w&usqp=CAU",
-			text: "Prueba 1"
-		},
-		{
-			src:
-				"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSzvIUbKcfOv5WH1RLxcq_Z9FPE38h0pXjEgw&usqp=CAU",
-			text: "Prueba 1"
-		},
-		{
-			src:
-				"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ7Vh0jU3tQOo0t9_BEAfS_ztfN4-RsyXj-_g&usqp=CAU",
-			text: "Prueba 1"
-		},
-		{
-			src:
-				"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQDwgWJoRxJOdrMhD_rJRGgTow3uvE3ZryOTA&usqp=CAU",
-			text: "Prueba 1"
-		}
-	];
-
 	return (
 		<div className="container-fluid">
 			<Navbar />
 
 			<Jumbotron title="A Warming Welcome!" />
 
-			<Card cards={myCardsArray} />
+			<Card />
 
 			<p className="text-center">
 				Made by <a href="http://www.4geeksacademy.com">Tibi</a>, with
